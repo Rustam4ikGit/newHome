5e390dcfaba50f6663eb51c463116c2db065d059
rustam <newmoonlife1@gmail.com>
2023-04-07 21:12:26 +0500

    ff

---
+++ js/mai.js
@@ -0,0 +1,47 @@
+                            //дз 2
+//1
+let str = 'aaa@bbb@ccc'
+
+console.log(
+    str.replace('@', '!',).replace('@', '!',)
+)
+//2
+let stR = "HELLO WORLD"
+
+console.log(
+    hello.slice(0,13) + "not JS"
+)
+//3
+let hello = 'Hello, it is HTML'
+
+console.log(
+    stR.slice(0,1).toUpperCase() + stR.slice(1).toLowerCase(),
+)
+//4
+let name = 'alex'
+
+console.log(
+    name.slice(0,1).toUpperCase() + name.slice(1)
+)
+//5
+console.log(Math.random()) 
+
+//6
+let text = "Lex Luter has a big suit"
+console.log(
+    text[text.search('a')].toUpperCase() +
+    text[text.search('L')] +
+    text[text.search('e')] +
+    text[text.search('x')] 
+)
+
+
+
+
+
+console.log(
+
+)
+
+
+
